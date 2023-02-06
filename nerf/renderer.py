61d01084c927eaeab20af568b6729874f2c4bd8c
Leo <leosegre@gmail.com>
2023-02-06 17:19:01 +0200

    lambda_p

--- nerf/renderer.py
+++ nerf/renderer.py
@@ -302,10 +302,10 @@ class NeRFRenderer(nn.Module):
             dnormaly_dy = torch.autograd.grad(normal[..., 1], xyzs, create_graph=True, grad_outputs=torch.ones_like(normal[..., 1]))[0][..., 1]
             dnormalz_dz = torch.autograd.grad(normal[..., 2], xyzs, create_graph=True, grad_outputs=torch.ones_like(normal[..., 2]))[0][..., 2]
 
-            mask = ~dnormalx_dx.isnan() & ~dnormalx_dy.isnan() & ~dnormalx_dz.isnan()
+            mask = ~dnormalx_dx.isnan() & ~dnormaly_dy.isnan() & ~dnormalz_dz.isnan()
             dnormalx_dx = dnormalx_dx[mask].reshape((-1, 1))
-            dnormaly_dy = dnormalx_dy[mask].reshape((-1, 1))
-            dnormalz_dz = dnormalx_dz[mask].reshape((-1, 1))
+            dnormaly_dy = dnormaly_dy[mask].reshape((-1, 1))
+            dnormalz_dz = dnormalz_dz[mask].reshape((-1, 1))
 
             if dnormalx_dx.shape[0] == 0:
                 poisson_loss_calc = torch.zeros(1, device=device)
