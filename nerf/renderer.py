9deb6ccfd18eb63c804ac799cfa46d16db8f16d7
Leo <leosegre@gmail.com>
2023-02-06 17:38:48 +0200

    test

--- nerf/renderer.py
+++ nerf/renderer.py
@@ -312,7 +312,8 @@ class NeRFRenderer(nn.Module):
             if dnormalx_dx.shape[0] == 0:
                 poisson_loss_calc = torch.zeros(1, device=device)
             else:
-                poisson_loss_calc = (dnormalx_dx**2 + dnormaly_dy**2 + dnormalz_dz**2).mean()
+                poisson_loss_calc = torch.zeros(1, device=device)
+                # poisson_loss_calc = (dnormalx_dx**2 + dnormaly_dy**2 + dnormalz_dz**2).mean()
             # print(sigmas.shape)
 
             # u_x = torch.autograd.functional.jacobian(self, (xyzs, dirs), create_graph=True)
